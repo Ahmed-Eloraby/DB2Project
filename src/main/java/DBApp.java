cfa190609b3807f801537c8ab25b83217c62807f
Ahmed Eloraby <ahmed.eloraby@student.guc.edu.eg>
2021-04-22 13:23:49 +0200

    Version 4

--- src/main/java/DBApp.java
+++ src/main/java/DBApp.java
@@ -1,15 +1,15 @@
 import java.io.*;
 import java.lang.reflect.Constructor;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Properties;
+import java.lang.reflect.InvocationTargetException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class DBApp implements DBAppInterface {
     static final int N = 200;
     HashMap<String, HashMap<String, String>> tableData = new HashMap<String, HashMap<String, String>>();
 
-    public static void main(String[] args) throws DBAppException {
+    public static void main(String[] args) throws DBAppException, NoSuchMethodException {
 //        String strTableName = "Student";
         DBApp dbApp = new DBApp();
 //        dbApp.init();
@@ -31,6 +31,8 @@ public class DBApp implements DBAppInterface {
 //
 //
 //        dbApp.createTable(strTableName, "id", htblColNameType, min, max);
+
+
     }
 
     @Override
@@ -86,12 +88,12 @@ public class DBApp implements DBAppInterface {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else
+        } else
             throw new DBAppException("Table name exists before");
 
     }
 
+    // Checking if a table exists in the database
     private boolean tableExists(String tableName) {
         try {
             BufferedReader br = new BufferedReader(new FileReader("src/main/resources/metadata.csv"));
@@ -132,8 +134,8 @@ public class DBApp implements DBAppInterface {
                     String[] line = current.split(",");
                     if (line[0].equals(tableName)) {
                         do {
-                            if(line[3].equals(true)){
-                                primaryKey=line[1];
+                            if (line[3].equals(true)) {
+                                primaryKey = line[1];
                             }
                             colType.put(line[1], line[2]);
                             colmin.put(line[1], line[6]);
@@ -154,31 +156,76 @@ public class DBApp implements DBAppInterface {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            if(!colNameValue.keySet().contains(primaryKey)){
+            if (!colNameValue.keySet().contains(primaryKey)) {
                 throw new DBAppException("Primary Key Should Have A Value");
             }
-            for(String columnName : colNameValue.keySet()){
-                Object value =colNameValue.get(columnName);
-                if(colType.get(columnName)==null){
-                   throw new DBAppException("Column Does not Exist");
+            for (String columnName : colNameValue.keySet()) {
+                Object value = colNameValue.get(columnName);
+                if (colType.get(columnName) == null) {
+                    throw new DBAppException("Column Does not Exist");
                 }
                 Class type = value.getClass();
-                if(!(type.getName().equals(colType.get(columnName)))){
+                if (!(type.getName().equals(colType.get(columnName)))) {
                     throw new DBAppException("Type Miss-match of Column: " + columnName);
                 }
-                Object min, max ;
-               try {
-                   Constructor constr = type.getConstructor(String.class);
+                Object min, max;
+                try {
+                    Constructor constr = type.getConstructor(String.class);
                     min = constr.newInstance(colmin.get(columnName));
                     max = constr.newInstance(colmax.get(columnName));
-               }
-               catch(Exception e){
+                    if (value instanceof java.lang.Integer) {
+                        int zvalue = (int) (value);
+                        int zmin = (int) (min);
+                        if (zvalue < zmin) {
+                            throw new DBAppException("Value Inserted is less than minimum allowed" + columnName);
 
-               }
-               //if(value.compareTo())
+                        }
+                        int zmax = (int) (max);
+                        if (zvalue > zmax) {
+                            throw new DBAppException("Value inserted is larger than maximum allowed" + columnName);
+                        }
+
+
+                    } else if (value instanceof java.lang.Double) {
+                        double zvalue = (double) (value);
+                        double zmin = (double) (min);
+                        if (zvalue < zmin) {
+                            throw new DBAppException("Value Inserted is less than minimum allowed for" + columnName);
+                        }
+                        double zmax = (double) (max);
+                        if (zvalue > zmax) {
+                            throw new DBAppException("Value inserted is larger than maximum allowed for" + columnName);
+                        }
+                    } else if (value instanceof java.lang.String) {
+                        String svalue = (String) (value);
+                        String smin = (String) (min);
+                        if ((svalue.compareTo(smin)) < 0) {
+                            throw new DBAppException("Value Inserted is less than minimum allowed for" + columnName);
+                        }
+                        String smax = (String) (max);
+                        if ((svalue.compareTo(smax)) > 0) {
+                            throw new DBAppException("Value inserted is larger than maximum allowed for" + columnName);
+                        }
+                    } else if (value instanceof java.util.Date) {
+                        SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date dvalue =sdformat.parse(String.valueOf(value));
+                        Date dmin = sdformat.parse(String.valueOf(min));
+                        Date dmax = sdformat.parse(String.valueOf(max));
+                        if (dvalue.compareTo(dmax) > 0) {
+                            throw new DBAppException("Date inserted Occurs after maximum allowable Date");
+                        } else if (dvalue.compareTo(dmin) < 0) {
+                            throw new DBAppException("Date inserted Occurs before minimum allowable Date");
+                        }
+                    }
+
+
+                } catch (Exception e) {
+
+                }
+                //if(value.compareTo())
             }
 
-        }else{
+        } else {
             throw new DBAppException("Table Does Not Exist");
         }
     }
